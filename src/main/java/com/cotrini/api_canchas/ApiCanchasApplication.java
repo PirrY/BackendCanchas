4272d1670554ac391864381665cda0db9fbb93be
Pirry <danielgiraldoval@gmail.com>
2026-04-01 09:17:43 -0500

    Inicializar proyecto Spring Boot con dependencias base

---
+++ src/main/java/com/cotrini/api_canchas/ApiCanchasApplication.java
@@ -0,0 +1,13 @@
+package com.cotrini.api_canchas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCanchasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCanchasApplication.class, args);
+	}
+
+}
